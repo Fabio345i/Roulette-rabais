9ab15d7574a22d8090adbe69a659fd83e4bee970
Fabio345i <famor002@edu.cegepgarneau.ca>
2025-01-05 20:09:15 -0500

    segments mashala

--- js/roue.js
+++ js/roue.js
@@ -1,6 +1,5 @@
 "use strict"
 
-const segmentAngles = [180, 90, 45, 45]; // 180 degrees for the first segment, smaller ones for the others
 let myWheel
 
 const courrielEsteban = "egagnon@qualiteetudiants.com"
@@ -10,12 +9,20 @@ const courrielJustine = "jpichette@qualiteetudiants.com"
 function creerRoue(){
     myWheel = new Winwheel({
         'canvasId': 'wheelCanvas',
-        'numSegments': segmentAngles.length, // Number of segments
+        'numSegments': 11,
         'segments': [
-            { 'fillStyle': '#eae56f', 'text': 'Large Prize', 'angle': segmentAngles[0] }, // Large segment (180 degrees)
-            { 'fillStyle': '#89f26e', 'text': 'Small Prize 1', 'angle': segmentAngles[1] },
-            { 'fillStyle': '#7de6ef', 'text': 'Small Prize 2', 'angle': segmentAngles[2] },
-            { 'fillStyle': '#e7706f', 'text': 'Small Prize 3' , 'angle': segmentAngles[3] }
+            { 'fillStyle': '#eae56f', 'text': '5% et lavage à pression'},
+            { 'fillStyle': '#eae56f', 'text': '5% et lavage à pression'},
+            { 'fillStyle': '#eae56f', 'text': '5% et lavage à pression'},
+            { 'fillStyle': '#eae56f', 'text': '5% et lavage à pression'},
+            { 'fillStyle': '#eae56f', 'text': '5% et lavage à pression'},
+            { 'fillStyle': '#eae56f', 'text': '5% et lavage à pression'},
+            { 'fillStyle': '#89f26e', 'text': '10%'},
+            { 'fillStyle': '#89f26e', 'text': '10%'},
+            { 'fillStyle': '#89f26e', 'text': '10%'},
+            { 'fillStyle': '#89f26e', 'text': '10%'},
+            { 'fillStyle': '#7de6ef', 'text': '15%'},
+            { 'fillStyle': '#7de6ef', 'text': '15%'}
         ],
         'animation': {
             'type': 'spinToStop',  // Spin animation
